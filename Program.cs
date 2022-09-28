f02cc790c1238cab71961a254bc086d7629a0cf7
yuliaokta <114564209+yuliaokta@users.noreply.github.com>
2022-09-27 19:36:10 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace literalvalues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("true");
+            Console.WriteLine("false");
+        }
+    }
+}
